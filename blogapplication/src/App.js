3661d38231efd3b1d6b9d4932d709b4f4b6cd73d
RenugaNatchimuthu <139890160+RenugaNatchimuthu@users.noreply.github.com>
2023-07-26 16:28:04 +0530

    Firebaseauthentication

--- blogapplication/src/App.js
+++ blogapplication/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import Createpost from "./Components/Createpost";
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+import { auth } from "./Components/firebase-config";
+import { useState } from "react";
+import {signOut} from "firebase/auth";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/"> Home </Link>
+
+        {!isAuth ? (
+          <Link to="/login"> Login </Link>
+        ) : (
+          <>
+            <Link to="/createpost"> Create Post </Link>
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home isAuth={isAuth} />} />
+        <Route path="/createpost" element={<Createpost isAuth={isAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ blogapplication/src/Components/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import{ getFirestore} from "firebase/firestore";
+import {getAuth,GoogleAuthProvider} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_ajgipUFMo1kCsCoV2hZXU7UNvZNK-eg",
+  authDomain: "blogapplication-a1e0e.firebaseapp.com",
+  projectId: "blogapplication-a1e0e",
+  storageBucket: "blogapplication-a1e0e.appspot.com",
+  messagingSenderId: "134053773527",
+  appId: "1:134053773527:web:cf54f8f4bd725ced587092"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
+export const auth =getAuth(app);
+export const provider=new GoogleAuthProvider();
